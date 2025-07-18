e4cbd8abb2fed1e98f09a23c8c061d5d0b4da14f
VinayMaan801 <dme@grindlaysindia.com>
2025-07-18 17:00:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,14 @@ const products = [
 { name: "CYL IR 2.75 (2545)", image: "images/CYL IR 2.75 (2545).png" },
 { name: "images/CYL IR 3 (234)", image: "images/CYL IR 3 (234).png" },
   { name: "CYL IR 3 (253) F Line", image: "images/CYL IR 3 (253) F Line.png" },
-
+  { name: "CYL IR 3 T Line", image: "images/CYL IR 3 T Line.png" },
+{ name: "CYL IR 3.5", image: "images/CYL IR 3.5.png" },
+{ name: "CYL IR 4 (242)", image: "images/CYL IR 4 (242).png" },
+{ name: "CYL IR 4 (2475)", image: "images/CYL IR 4 (2475).png" },
+{ name: "CYL IR 5 (253) F Line", image: "images/CYL IR 5 (253) F Line.png" },
+{ name: "CYL IR 5 2545", image: "images/CYL IR 5 2545.png" },
+  { name: "CYL IR 5 T Line", image: "images/CYL IR 5 T Line.png" },
+{ name: "CYL IR 5.5", image: "images/CYL IR 5.5.png" },
   
   { name: "Demo", image: "images/CYL IR 1.75 (234).png" },
   { name: "Demo", image: "images/CYL IR 1.75 (234).png" }
