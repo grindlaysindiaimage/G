70b6a39644af528fd7625c4de4a2bf85db13367f
Maan <dme@grindlaysindia.com>
2025-09-01 16:43:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -376,7 +376,7 @@ const products = [
     { name: "IR 5.5 M-TYPE / O.E-TYPE (2+2),(2+1) STD", image: "images/5 & 5.5.jpeg", category: "Piston Ring"  },
     { name: "IR 5.5 KADA/PLAIN TYPE (2+2) STD", image: "images/5 & 5.5 plain type.jpeg", category: "Piston Ring"  },
 
-{ name: "OIL SEAL.png", image: "images/Blank.png", category: "Oil Seal"  },  
+{ name: "OIL SEAL.png", image: "images/OIL SEAL.png", category: "Oil Seal"  },  
 { name: "32x45x7", image: "images/32x45x7.jpeg", category: "Oil Seal"  },
   { name: "45x60x12", image: "images/45-60-12.jpeg", category: "Oil Seal"  }, 
   { name: "60x85x10", image: "images/60x85x10.jpeg", category: "Oil Seal"  },
